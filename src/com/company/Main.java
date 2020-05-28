4db057ec9cdb1c6e0f5d759bdd4ff233464c3769
Sergey Solomatin <solomatin.vrn@gmail.com>
2020-05-28 20:44:02 +0300

    hello-world

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+
+        System.out.println(hello + " "+ world + "!");
+    }
+}
